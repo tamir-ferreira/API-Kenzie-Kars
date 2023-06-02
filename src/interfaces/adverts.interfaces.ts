7d07dff62aeb939e3e4cefbd81a47e0fefb19ea5
LuccaHaddadSerejo <hslucca@gmail.com>
2023-06-02 10:49:19 -0300

    feat: create migrations folder, entity and base folders/files for adverts table

---
+++ src/interfaces/adverts.interfaces.ts
@@ -0,0 +1,2 @@
+import { z } from 'zod';
+import { DeepPartial } from 'typeorm';
---
+++ src/routers/adverts.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const advertsRoutes = Router();
+
+export default advertsRoutes;
