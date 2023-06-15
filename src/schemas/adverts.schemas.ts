6e2231b3f90945a312ec2c55c535ed49db51f82f
Tamir Ferreira <bigtam_rj@yahoo.com.br>
2023-06-15 15:55:29 -0300

    feat: update adverts (entity/schema)

--- src/schemas/adverts.schemas.ts
+++ src/schemas/adverts.schemas.ts
@@ -2,15 +2,18 @@ import { z } from "zod";
 
 const advertSchema = z.object({
   id: z.number(),
-  title: z.string().max(150),
-  description: z.string().optional().nullish(),
-  price: z.number().or(z.string()),
-  createdAt: z.string(),
-  updatedAt: z.string(),
   brand: z.string().max(40),
   model: z.string().max(40),
   year: z.number(),
   fuel: z.string().max(10),
+  mileage: z.number(),
+  color: z.string().max(20),
+  fipe_price: z.number().or(z.string()),
+  price: z.number().or(z.string()),
+  description: z.string(),
+  cover_image: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
   is_active: z.boolean().optional().default(true),
 });
 
@@ -24,9 +27,4 @@ const advertSchemaMultiple = z.array(advertSchema);
 
 const advertSchemaUpdate = advertSchemaRequest.partial();
 
-export {
-  advertSchema,
-  advertSchemaRequest,
-  advertSchemaMultiple,
-  advertSchemaUpdate,
-};
+export { advertSchema, advertSchemaRequest, advertSchemaMultiple, advertSchemaUpdate };
