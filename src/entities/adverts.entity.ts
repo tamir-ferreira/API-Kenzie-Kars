d5a29de89b688ef8e7e6b7db592be69888a59bc3
Tamir Ferreira <bigtam_rj@yahoo.com.br>
2023-06-20 18:33:04 -0300

    feat: capitalize firt letter adverts

--- src/entities/adverts.entity.ts
+++ src/entities/adverts.entity.ts
@@ -5,6 +5,8 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  BeforeInsert,
+  BeforeUpdate,
 } from "typeorm";
 import { User } from "./users.entity";
 
@@ -54,4 +56,19 @@ export class Advert {
 
   @ManyToOne(() => User, (user) => user.adverts, { onDelete: "CASCADE" })
   user: User;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  capitalizeBrandAndModel() {
+    if (this.brand) {
+      this.brand = this.capitalizeFirstLetter(this.brand);
+    }
+    if (this.model) {
+      this.model = this.capitalizeFirstLetter(this.model);
+    }
+  }
+
+  private capitalizeFirstLetter(value: string): string {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
 }
