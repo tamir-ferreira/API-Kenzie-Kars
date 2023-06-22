3161f6022128197092e2ccd91d91241b08db1228
AntonioAugustoRezende <antoniorezende7@outlook.com>
2023-06-22 01:47:58 -0300

    feat: logic params

--- src/services/adverts/readAdverts.service.ts
+++ src/services/adverts/readAdverts.service.ts
@@ -3,16 +3,43 @@ import { Repository } from "typeorm";
 import { tAdvertMultiple } from "../../interfaces/adverts.interfaces";
 import { advertSchemaMultiple } from "../../schemas/adverts.schemas";
 import { Advert } from "../../entities/adverts.entity";
+import { log } from "console";
 
-const readAdvertsService = async (): Promise<tAdvertMultiple> => {
+const readAdvertsService = async (
+  brand: any,
+  model: any,
+  color: any,
+  year: any,
+  fuel: any
+): Promise<tAdvertMultiple> => {
   const advertRepository: Repository<Advert> =
     AppDataSource.getRepository(Advert);
 
-  const adverts: Array<Advert> = await advertRepository.find({
-    relations: {
-      user: true,
-    },
-  });
+  let adverts: any;
+
+  console.log(model);
+  console.log(brand);
+
+  if (brand == "" && model == "" && color == "" && year == "" && fuel == "") {
+    adverts = await advertRepository.find({
+      relations: {
+        user: true,
+      },
+    });
+  } else {
+    adverts = await advertRepository.find({
+      relations: {
+        user: true,
+      },
+      where: {
+        brand: brand !== "" ? brand : null,
+        model: model !== "" ? model : null,
+        color: color !== "" ? color : null,
+        year: year !== "" ? year : null,
+        fuel: fuel !== "" ? fuel : null,
+      },
+    });
+  }
 
   return advertSchemaMultiple.parse(adverts);
 };
